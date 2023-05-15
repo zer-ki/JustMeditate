971ece2280732b787fc88e57dfe6cd8574cf7505
Filip <filipkarcz01@gmail.com>
2023-05-15 08:44:54 +0200

    Add click listener for start button

--- app/src/main/java/com/example/justmeditate/MainActivity.kt
+++ app/src/main/java/com/example/justmeditate/MainActivity.kt
@@ -1,11 +1,19 @@
 package com.example.justmeditate
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        title = "Just Meditate"
+        val button: Button = findViewById(R.id.start_button)
+        button.setOnClickListener {
+            val intent = Intent(this@MainActivity, InterfaceTimer::class:java)
+            startActivity(intent)
+        }
     }
 }
