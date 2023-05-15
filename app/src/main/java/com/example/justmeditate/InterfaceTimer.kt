c991ea7518a9682fc5e7e0dacd6c7110722f85b5
Filip <filipkarcz01@gmail.com>
2023-05-15 09:00:52 +0200

    Implement stop button

--- app/src/main/java/com/example/justmeditate/InterfaceTimer.kt
+++ app/src/main/java/com/example/justmeditate/InterfaceTimer.kt
@@ -1,12 +1,20 @@
 package com.example.justmeditate
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.coroutines.NonCancellable.start
 
 class InterfaceTimer : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.timer_interface)
-
+        title="Meditating"
+        val button: Button = findViewById(R.id.stop_button)
+        button.setOnClickListener {
+            val intent = Intent(this@InterfaceTimer, MainActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
