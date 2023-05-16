b775f360746c4838b0f80de69e04e043d762df4c
Filip <filipkarcz01@gmail.com>
2023-05-16 09:05:47 +0200

    Add NumberPicker functions to MainActivity

--- app/src/main/java/com/example/justmeditate/MainActivity.kt
+++ app/src/main/java/com/example/justmeditate/MainActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.NumberPicker
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,5 +16,24 @@ class MainActivity : AppCompatActivity() {
             val intent = Intent(this@MainActivity, InterfaceTimer::class.java)
             startActivity(intent)
         }
+
+    }
+    fun setHourPicker(){
+        val hourPicker: NumberPicker = findViewById(R.id.picker_hour)
+        hourPicker.minValue = 0
+        hourPicker.maxValue = 24
+        hourPicker.wrapSelectorWheel = true
+    }
+    fun setMinutePicker(){
+        val minutePicker: NumberPicker = findViewById(R.id.picker_minute)
+        minutePicker.minValue = 0
+        minutePicker.maxValue = 59
+        minutePicker.wrapSelectorWheel = true
+    }
+    fun setSecondPicker(){
+        val secondPicker: NumberPicker = findViewById(R.id.picker_second)
+        secondPicker.minValue = 0
+        secondPicker.maxValue = 59
+        secondPicker.wrapSelectorWheel = true
     }
 }
