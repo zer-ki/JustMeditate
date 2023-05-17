d2cda969d541ddd616e8f7af3aacacf683eaace5
Filip <filipkarcz01@gmail.com>
2023-05-17 09:12:06 +0200

    Move timer into function

--- app/src/main/java/com/example/justmeditate/InterfaceTimer.kt
+++ app/src/main/java/com/example/justmeditate/InterfaceTimer.kt
@@ -7,10 +7,12 @@ import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import android.widget.TextView
 import java.util.concurrent.TimeUnit
+import kotlin.properties.Delegates
 
 class InterfaceTimer : AppCompatActivity() {
     lateinit var textView : TextView
     private lateinit var timer: CountDownTimer
+    private var timeLeftInMilis by Delegates.notNull<Int>()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.timer_interface)
@@ -19,27 +21,8 @@ class InterfaceTimer : AppCompatActivity() {
         val hours = intent.getIntExtra("hour", 0)
         val minutes = intent.getIntExtra("minute", 0)
         val seconds = intent.getIntExtra("second", 0)
-            timer = object : CountDownTimer(hours*3600000+minutes*60000+seconds*1000.toLong(), 1000) {
-                override fun onTick(millis: Long) {
-                    val hms = String.format("%02d:%02d:%02d",
-                        (TimeUnit.MILLISECONDS.toHours(millis) - TimeUnit.DAYS.toHours(
-                            TimeUnit.MILLISECONDS.toDays(
-                                millis
-                            )
-                        )),
-                        (TimeUnit.MILLISECONDS.toMinutes(millis) -
-                                TimeUnit.HOURS.toMinutes(TimeUnit.MILLISECONDS.toHours(millis))),
-                        (TimeUnit.MILLISECONDS.toSeconds(millis) - TimeUnit.MINUTES.toSeconds(
-                            TimeUnit.MILLISECONDS.toMinutes(millis)
-                        ))
-                    )
-                    textView.text = (hms)
-                }
-
-                override fun onFinish() {
-                    textView.text = "Finished!"
-                }
-            }
+        timeLeftInMilis = (hours*3600000+minutes*60000+seconds*1000)
+        startTimer()
 
         val button: Button = findViewById(R.id.button_stop)
         button.setOnClickListener {
@@ -48,7 +31,29 @@ class InterfaceTimer : AppCompatActivity() {
         }
 
     }
+    private fun startTimer(){
+        timer = object : CountDownTimer(timeLeftInMilis.toLong(), 1000) {
+            override fun onTick(millis: Long) {
+                val hms = String.format("%02d:%02d:%02d",
+                    (TimeUnit.MILLISECONDS.toHours(millis) - TimeUnit.DAYS.toHours(
+                        TimeUnit.MILLISECONDS.toDays(
+                            millis
+                        )
+                    )),
+                    (TimeUnit.MILLISECONDS.toMinutes(millis) -
+                            TimeUnit.HOURS.toMinutes(TimeUnit.MILLISECONDS.toHours(millis))),
+                    (TimeUnit.MILLISECONDS.toSeconds(millis) - TimeUnit.MINUTES.toSeconds(
+                        TimeUnit.MILLISECONDS.toMinutes(millis)
+                    ))
+                )
+                textView.text = (hms)
+            }
 
+            override fun onFinish() {
+                textView.text = "Finished!"
+            }
+        }
+    }
     override fun onStart() {
         super.onStart()
         timer.start()
