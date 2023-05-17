2790a2037fe73f164a69a31d745d87e5232bc8c6
Filip <filipkarcz01@gmail.com>
2023-05-17 08:17:28 +0200

    Fix argument passing bug

--- app/src/main/java/com/example/justmeditate/InterfaceTimer.kt
+++ app/src/main/java/com/example/justmeditate/InterfaceTimer.kt
@@ -16,28 +16,29 @@ class InterfaceTimer : AppCompatActivity() {
         setContentView(R.layout.timer_interface)
         textView = findViewById(R.id.timer_text)
         title="Meditating"
-        val hours = intent.getIntExtra("hour",10)
-        timer = object : CountDownTimer((hours*10000).toLong(), 1000) {
-            override fun onTick(millis: Long) {
-                val hms = String.format("%02d:%02d:%02d",
-                    (TimeUnit.MILLISECONDS.toHours(millis) - TimeUnit.DAYS.toHours(
-                    TimeUnit.MILLISECONDS.toDays(
-                        millis
+        val hours = intent.getIntExtra("hour", 0)
+            timer = object : CountDownTimer(hours*1000.toLong(), 1000) {
+                override fun onTick(millis: Long) {
+                    val hms = String.format("%02d:%02d:%02d",
+                        (TimeUnit.MILLISECONDS.toHours(millis) - TimeUnit.DAYS.toHours(
+                            TimeUnit.MILLISECONDS.toDays(
+                                millis
+                            )
+                        )),
+                        (TimeUnit.MILLISECONDS.toMinutes(millis) -
+                                TimeUnit.HOURS.toMinutes(TimeUnit.MILLISECONDS.toHours(millis))),
+                        (TimeUnit.MILLISECONDS.toSeconds(millis) - TimeUnit.MINUTES.toSeconds(
+                            TimeUnit.MILLISECONDS.toMinutes(millis)
+                        ))
                     )
-                )),
-                    (TimeUnit.MILLISECONDS.toMinutes(millis) -
-                            TimeUnit.HOURS.toMinutes(TimeUnit.MILLISECONDS.toHours(millis))),
-                    (TimeUnit.MILLISECONDS.toSeconds(millis) - TimeUnit.MINUTES.toSeconds(
-                        TimeUnit.MILLISECONDS.toMinutes(millis)
-                    ))
-                )
-                textView.text = (hms)
+                    textView.text = (hms)
+                }
+
+                override fun onFinish() {
+                    textView.text = "Finished!"
+                }
             }
 
-            override fun onFinish() {
-                textView.text = "Finished!"
-            }
-        }
         val button: Button = findViewById(R.id.stop_button)
         button.setOnClickListener {
             val intent = Intent(this@InterfaceTimer, MainActivity::class.java)
--- app/src/main/java/com/example/justmeditate/MainActivity.kt
+++ app/src/main/java/com/example/justmeditate/MainActivity.kt
@@ -15,27 +15,30 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         title = "Just Meditate"
 
-
         hourPicker = findViewById(R.id.picker_hour)
         hourPicker.minValue = 0
         hourPicker.maxValue = 24
         hourPicker.wrapSelectorWheel = true
-        var hour = hourPicker.value
 
         minutePicker = findViewById(R.id.picker_minute)
         minutePicker.minValue = 0
         minutePicker.maxValue = 59
         minutePicker.wrapSelectorWheel = true
-        var minute = minutePicker.value
 
         secondPicker = findViewById(R.id.picker_second)
         secondPicker.minValue = 0
         secondPicker.maxValue = 59
         secondPicker.wrapSelectorWheel = true
-        var second = secondPicker.value
 
         val button: Button = findViewById(R.id.start_button)
         button.setOnClickListener {
+
+            val hour = hourPicker.value
+
+            val minute = minutePicker.value
+
+            val second = secondPicker.value
+
             val intent = Intent(this@MainActivity, InterfaceTimer::class.java)
             intent.putExtra("hour", hour)
             intent.putExtra("minute", minute)
